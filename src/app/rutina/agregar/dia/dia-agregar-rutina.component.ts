6ce0e78a3f7a3106c357546d27dc2840ef9462b5
SebastianBellaIt <70810659+SebastianBellaIt@users.noreply.github.com>
2023-11-01 22:47:02 -0300

    comit del front terminado a revisar de la rutina preset

--- src/app/rutina/agregar/dia/dia-agregar-rutina.component.ts
+++ src/app/rutina/agregar/dia/dia-agregar-rutina.component.ts
@@ -1,18 +1,50 @@
-import { Component } from '@angular/core';
+import { Component    } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { Input        } from '@angular/core';
 import { Output       } from '@angular/core';
+import { Ejercicio, EjercicioAgregarRutinaComponent } from '../ejercicio/ejercicio-agregar-rutina.component';
 
+export class Dia {
+	idTipoEjercicio! : number;
+	series! : number;
+	repeticiones! : string;
+	diaRutina!: number;
+	numeroDia!:number;
+
+}
 @Component({
-  selector: 'app-dia-agregar-rutina',
-  templateUrl: './dia-agregar-rutina.component.html',
-  styleUrls: ['./dia-agregar-rutina.component.scss']
+	selector    : 'app-dia-agregar-rutina',
+	templateUrl : './dia-agregar-rutina.component.html',
+	styleUrls   : ['./dia-agregar-rutina.component.scss']
 })
 export class DiaAgregarRutinaComponent {
-  public registroEjercicios : Array<any> = [1];
-  @Input()
-  public numeroDia : number = 1;
-  public agregarEjercicio() :void{
-    let nuevoEjer : number = this.registroEjercicios.length + 1;
-    this.registroEjercicios.push(nuevoEjer);
-  }
+
+
+	@Input()
+	public numeroDia : number = 1;
+
+	@Input()
+	public ejercicios : Array<any> = [];
+
+	public registroEjercicios : Array<any> = [];
+
+	ngOnInit(){
+		this.agregarEjercicio();
+	}
+
+
+	public eliminarEjercicio(index:number): void{
+		this.ejercicios.splice(index,1);
+	}
+
+	public agregarEjercicio() :void {
+		let ejercicio : Ejercicio = new Ejercicio();
+		ejercicio.diaRutina = this.numeroDia;
+		this.ejercicios.push(ejercicio);
+	}
+
+	@Output()
+	public clickBotonAgregarEjercicio : EventEmitter <void> = new EventEmitter();
+	@Output()
+	public clickBotonEliminarDia : EventEmitter<void> = new EventEmitter();
 }
