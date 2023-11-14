f746f857027bcf8b04588d84e5283fa3801e228f
Superalst <agustinrojas99@hotmail.com>
2023-11-14 01:28:07 -0300

    Termine la muestra de datos cuando ingresa el DNI, falta ver el agregar y dehabilitar el boton de agregar

--- src/app/cuota/agregar/cuota-agregar.component.ts
+++ src/app/cuota/agregar/cuota-agregar.component.ts
@@ -37,7 +37,6 @@ export class CuotaAgregarComponent extends FormularioBaseComponent {
 			telefono: new FormControl({ value: '', disabled: true }),
 			fecha_desde: new FormControl({ value: '', disabled: true }),
 			monto: new FormControl({ value: '', disabled: true }),
-
 		});
 	}
 
@@ -71,6 +70,16 @@ export class CuotaAgregarComponent extends FormularioBaseComponent {
 	public async clickValidarDniSocio() : Promise<void> {
 		try {
 			let response= await this.apiService.post("/cuota-mensual/validar-pago",{dni:this.form.get("dni")?.value})
+			this.form = this.formBuilder.group({
+				dni: new FormControl			 ({ value: response.data.socio.dni, disabled: false }),
+				nombre: new FormControl		 	 ({ value: response.data.socio.nombre, disabled: true }),
+				apellido: new FormControl		 ({ value: response.data.socio.apellido, disabled: true }),
+				fecha_nacimiento: new FormControl({ value: response.data.socio.fecha_nacimiento, disabled: true }),
+				telefono: new FormControl		 ({ value: response.data.socio.telefono, disabled: true }),
+				fecha_desde: new FormControl	 ({ value: response.data.precio_cuota.fecha_desde, disabled: true }),
+				monto: new FormControl			 ({ value: response.data.precio_cuota.monto, disabled: true }),
+	
+			});
 		} catch (error) {
 			console.log(error)
 		}
