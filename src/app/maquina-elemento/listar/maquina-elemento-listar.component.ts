70b24e898a8153854af80e47caf7bf39f24e25c4
SebastianBellaIt <70810659+SebastianBellaIt@users.noreply.github.com>
2023-11-15 02:31:06 -0300

    front de maquina elemento, tipo ejerciio y logn de caracteres en algunos forms

--- src/app/maquina-elemento/listar/maquina-elemento-listar.component.ts
+++ src/app/maquina-elemento/listar/maquina-elemento-listar.component.ts
@@ -10,27 +10,29 @@ export class MaquinaElementoListarComponent extends ListadoBaseComponent {
 
 	public registrosListado : Array<any> = [];
 
-	public columnasAMostrar : Array<string> = ["id", "descripcion", "estado", "editar", "eliminar"];
+	public columnasAMostrar : Array<string> = ["id", "descripcion", "editar", "eliminar"];
 
 	ngOnInit() : void {	
+
 		this.obtenerListado();
 	}
 
 	private async obtenerListado() : Promise<void> {
 		this.registrosListado = await this.apiService.getData("/maquina-elemento/listar");
 	}
-
 	public clickBotonEditar(id : number) : void {
 		this.router.navigate([`maquina-elemento/${id}/editar`]);
 	}
 
 	public async clickBotonEliminar(id : number) : Promise<void> {
+		let resback : string = ''
 		let respuesta = await this.confirmService.mostrarMensajeConfirmacion(
 			"¿Estás seguro que quieres eliminar esta máquina/elemento?",
 			"Eliminar"
 		);
 		if(respuesta){
-			await this.apiService.post(`/maquina-elemento/${id}/eliminar`, {});
+			resback = await this.apiService.getData(`/maquina-elemento/${id}/eliminar`, {});
+			await this.confirmService.mostrarMensajeConfirmacion(resback);
 			this.obtenerListado();
 		}
 	}
